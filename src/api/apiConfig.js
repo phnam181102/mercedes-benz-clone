20ccf7e91c75f5102f9d3454dde8eaa880287d5f
Nam Phan <phnam181102@gmail.com>
2022-11-16 15:32:28 +0700

    update

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
-
-export default apiConfig;
