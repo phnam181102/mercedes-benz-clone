c9123a4cb62de82dbd483524002687ff1e51e8cc
Nam Phan <phnam181102@gmail.com>
2023-07-12 20:43:55 +0700

    update

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -41,7 +41,7 @@ function Register() {
         }
 
         try {
-            const res = await axios.post('http://localhost:5000/api/users/register', {
+            const res = await axios.post('http://localhost:3000/api/users/register', {
                 username,
                 email,
                 password,
